4004b6593566a594e37657a5950c579afaba3fda
Matt Thommes <matt@activecampaign.com>
2015-04-26 17:49:12 -0500

    Building out more components for testing HTML pages

--- node_test/app.js
+++ node_test/app.js
@@ -1,30 +1,80 @@
 #!/bin/env node
-var express = require("express");
-var request = require("request");
-var RSVP = require("rsvp");
-var ActiveCampaign = require("activecampaign");
+(function($) {
 
-var Test = function() {
+	var express = require("express");
+	var cons = require("consolidate");
+	//var request = require("request");
+	var RSVP = require("rsvp");
+	var ActiveCampaign = require("activecampaign");
 
-	var self = this;
-	self.ipaddress = "localhost";
-	self.port = 8082;
+	var Test = function() {
 
-	self.server = function() {
-		self.app = express();
-		self.app.listen(self.port, self.ipaddress, function() {        
-			console.log('%s: Node server started on %s:%d ...',
-				Date(Date.now()), self.ipaddress, self.port);
-		});
-	};
+		var self = this;
+		self.ipaddress = "localhost";
+		self.port = process.env.port || 8082;
 
-	self.activecampaign = function(api_url, api_key) {
-		var ac = new ActiveCampaign(api_url, api_key);
-		ac.debug = true;
+		self.create_routes = function() {
+			self.routes = {};
+			self.routes["/"] = {method: "get", callback: function(req, res) {
+				res.render("index", {
+					title: "My title",
+					test1: "test"
+				});
+			}};
+		}
+
+		self.terminator = function(sig) {
+			if (typeof sig === "string") {
+				console.log('%s: Received %s - terminating sample app ...',
+					Date(Date.now()), sig);
+				process.exit(1);
+			}
+			console.log('%s: Node server stopped.', Date(Date.now()));
+		};
+
+		self.setup_terminate = function() {
+			process.on('exit', function(err) {
+				throw new Error("process is exiting");
+				self.terminator();
+			});
+		}
+
+		self.initialize = function() {
+			self.create_routes();
+			self.app = express();
+			self.app.engine("html", cons.handlebars);
+			self.app.set("view engine", "html");
+			self.app.set("views", __dirname + "/views");
+			for (var r in self.routes) {
+				if (self.routes[r].method == "get") {
+					self.app.get(r, self.routes[r].callback);
+				}
+				else if (self.routes[r].method == "post") {
+					self.app.post(r, self.routes[r].callback);
+				}
+			}
+			//self.setup_terminate();
+			//self.server();
+		}
+
+		self.server = function() {
+			self.app.listen(self.port, self.ipaddress, function() {        
+				console.log('%s: Node server started on %s:%d ...',
+					Date(Date.now()), self.ipaddress, self.port);
+			});
+		};
+
+		self.activecampaign = function(api_url, api_key) {
+			self.ac = new ActiveCampaign(api_url, api_key);
+			//self.ac.debug = true;
+		}
+
 	}
 
-}
+	var test = new Test();
 
-var test = new Test();
-//test.server();
-test.activecampaign("", "");
+	// If this produces "EADDRINUSE", run `ps aux | grep node`, then `kill -9 PID`
+	test.initialize();
+	//test.activecampaign("", "");
+
+})();
