adfac69a9accdcf995cf09dbb737c60c14884f5c
Matt Thommes <matt@activecampaign.com>
2016-02-16 09:27:57 -0600

    Output the raw date being converted.

--- datetime.php
+++ datetime.php
@@ -9,16 +9,18 @@ print_r($current_utc);
 
 echo "<hr>";
 
-echo "Chicago time for supplied UTC timestamp:<br>";
+$test1 = "01:56:36 UTC 2016-02-04";
+echo "Chicago time for supplied UTC timestamp ({$test1}):<br>";
 date_default_timezone_set("America/Chicago");
-$x = date("Y-m-d H:i:s", strtotime("01:56:36 UTC 2016-02-04"));
+$x = date("Y-m-d H:i:s", strtotime($test1));
 print_r($x);
 
 echo "<hr>";
 
-echo "Australia time for supplied SQL timestamp:<br>";
+$test2 = "2016-02-07 22:19:06";
+echo "Australia time for supplied SQL timestamp ({$test2}):<br>";
 date_default_timezone_set("Australia/Sydney");
-$x = date("Y-m-d H:i:s", strtotime("2016-02-07 22:19:06"));
+$x = date("Y-m-d H:i:s", strtotime($test2));
 print_r($x);
 
 ?>
